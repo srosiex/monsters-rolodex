7267c055bdc681ede8673d2a6f746722f5668969
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-03-08 23:10:07 -0500

    Updates

---
+++ precache-manifest.6a18c7b92b49404b80886e8c5176fdb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3947182edb0b32113832fe17c4fd1427",
+    "url": "/https//srosiex.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ec8fa85f8b112979531",
+    "url": "/https//srosiex.github.io/monsters-rolodex/static/css/main.10dd472f.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/https//srosiex.github.io/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/https//srosiex.github.io/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec8fa85f8b112979531",
+    "url": "/https//srosiex.github.io/monsters-rolodex/static/js/main.cefac33c.chunk.js"
+  },
+  {
+    "revision": "3e92813207951734fc23",
+    "url": "/https//srosiex.github.io/monsters-rolodex/static/js/runtime-main.923c4f27.js"
+  }
+]);
